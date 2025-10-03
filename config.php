4e663b17402e464ba29dc36ec8125ad0e6150349
ArtemKochanov <artem06092001@yandex.ru>
2025-10-03 08:41:26 +0000

    1

--- config.php
+++ config.php
@@ -28,6 +28,8 @@ require_once(__DIR__ . '/lib/setup.php');
 $CFG->session_handler_class = '\core\session\redis';
 $CFG->session_redis_host = '127.0.0.1';
 $CFG->session_redis_port = 6379;
+$CFG->seesion_redis_database = 1;
+$CFG->session_redis_prefix = 'moodle_';
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
